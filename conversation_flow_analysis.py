c7d3e9100db57052fb7ff5e9fdf8da422b9de2bb
Lucian Istrati <lucianistrati@yahoo.com>
2024-10-25 11:19:12 +0200

    Delete conversation_flow_analysis.py

--- conversation_flow_analysis.py
+++
@@ -1,5 +0,0 @@
-""""
-Conversation Flow Analysis:
-
-Dialogflow or Rasa NLU: These tools can help analyze the flow of conversation and identify key points where the potential client may have hesitations or concerns.
-"""
