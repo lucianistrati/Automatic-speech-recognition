925bbe24178e24a03ef978dbce3fa7795e7f0742
Lucian Istrati <lucianistrati@yahoo.com>
2024-10-25 11:13:33 +0200

    Delete emotion_analysis.py

--- emotion_analysis.py
+++
@@ -1,7 +0,0 @@
-"""
-Emotion Analysis:
-IBM Watson Tone Analyzer or Affectiva: These tools can help the seller understand the emotional state of the potential client during the conversation.
-
-"""
-
-
